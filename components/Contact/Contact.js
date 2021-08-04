db29fc5cb767518d27b1835c48f7796357caa5e2
matchi01 <mateusz.w.twardy@gmail.com>
2021-08-04 20:37:24 +0200

    Added Contact component

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,7 +1,25 @@
 import styled from "styled-components";
+import {Section, Subtitle, Title} from "../../styles/GlobalComponents";
+import {HeroLink, HeroLinksList, HeroWrapper} from "../Hero/HeroStyles";
 
-const $Title$ = () => (
-
+const Hero = () => (
+    <Section>
+        <HeroWrapper>
+            <Title>
+                Hi! I am Mati.
+            </Title>
+            <Subtitle>
+                Junior Fullstack Developer
+            </Subtitle>
+            <HeroLinksList>
+                <HeroLink to="/notes"> ABOUT </HeroLink> /
+                <HeroLink to="/notes"> PROJECTS </HeroLink> /
+                <HeroLink to="/notes"> CONTACT </HeroLink>
+            </HeroLinksList>
+        </HeroWrapper>
+    </Section>
 );
+
+export default Hero;
 
 export default $Title$;
