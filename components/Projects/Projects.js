81daa394736a650a41ebe4da9093f996ea3dc653
matchi01 <mateusz.w.twardy@gmail.com>
2021-08-04 22:54:11 +0200

    Work on project cards

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import {Section, SectionDivider, SectionTitle, Subtitle, Title} from "../../styles/GlobalComponents";
 import {HeroLink, HeroLinksList, HeroWrapper} from "../Hero/HeroStyles";
+import coon from '../../assets/coon.png'
+
 import {
     BlogCard,
     CardInfo,
@@ -13,23 +15,22 @@ import {
     UtilityList
 } from "./ProjectsStyles";
 import React from "react";
-import {projects} from "./constans";
 
 const Projects = () => (
     <ProjectsSection nopadding id="projects">
         <Title>Projekty</Title>
         <GridContainer>
-            <BlogCard key={i}>
-                <Img src={}/>
+            <BlogCard >
+                <Img src={coon}/>
                 <TitleContent>
-                    <HeaderThree title>{p.title}</HeaderThree>
+                    <HeaderThree title>Tytuł projektu</HeaderThree>
                     <Hr/>
                 </TitleContent>
-                <CardInfo className="card-info">{p.description}</CardInfo>
+                <CardInfo className="card-info">Jakiś krótki i konkretny opis</CardInfo>
 
                 <UtilityList>
-                    <ExternalLinks href={p.visit}>Code</ExternalLinks>
-                    <ExternalLinks href={p.source}>Source</ExternalLinks>
+                    <ExternalLinks href="www.google.pl">Code</ExternalLinks>
+                    <ExternalLinks href="www.google.pl">Source</ExternalLinks>
                 </UtilityList>
             </BlogCard>
         </GridContainer>
--- components/Projects/ProjectsStyles.js
+++ components/Projects/ProjectsStyles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Image from 'next/image'
 
 export const ProjectsSection = styled.section`
     //display: ${(props) => props.grid ? "grid" : "flex"};
@@ -29,10 +30,10 @@ export const GridContainer = styled.section`
 `
 
 
-export const Img = styled.img`
-  width:100%;
-  height:100%;
-  object-fit: cover;
+export const Img = styled(Image)`
+  //width:80%;
+  //height:80%;
+  //object-fit: cover;
   overflow: hidden;
 `
 
@@ -40,7 +41,7 @@ export const BlogCard = styled.div`
   border-radius: 10px;
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
-  //width: 400px;
+  max-width: 400px;
 `;
 export const TitleContent = styled.div`
   text-align: center;
