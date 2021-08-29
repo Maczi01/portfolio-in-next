b7cfff5eb127f32cda0dae95addfea4126083ff5
matchi01 <mateusz.w.twardy@gmail.com>
2021-08-29 23:56:09 +0200

    Removed some css properties

--- components/About/SkillsSvgWrapper.js
+++ components/About/SkillsSvgWrapper.js
@@ -5,8 +5,4 @@ export const SkillsSvgWrapper = styled.div`
     width: 50px;
     height: 50px;
     color: ${({theme}) => theme.colors.background2};
-    // transition: color 4s 1.5s;
-    // overflow: hidden;
-
-
 `
